84a2d42dfc1993514278c02973b1b617eea3032d
troller2705 <cole.groves@torontofilmschool.ca>
2025-07-14 10:48:26 -0400

    Update SubscriptionDepositorMenu.java

--- src/main/java/com/troller2705/createnumismaticssubs/content/subscription_depositor/SubscriptionDepositorMenu.java
+++ src/main/java/com/troller2705/createnumismaticssubs/content/subscription_depositor/SubscriptionDepositorMenu.java
@@ -3,13 +3,10 @@ package com.troller2705.createnumismaticssubs.content.subscription_depositor;
 import com.simibubi.create.foundation.gui.menu.MenuBase;
 import dev.ithundxr.createnumismatics.content.backend.Coin;
 import dev.ithundxr.createnumismatics.content.bank.CardSlot;
-import dev.ithundxr.createnumismatics.content.coins.CoinDisplaySlot;
 import dev.ithundxr.createnumismatics.content.coins.CoinItem;
-import dev.ithundxr.createnumismatics.content.coins.SlotDiscreteCoinBag;
 import dev.ithundxr.createnumismatics.registry.NumismaticsTags;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
@@ -19,6 +16,8 @@ import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public class SubscriptionDepositorMenu extends MenuBase<SubscriptionDepositorBlockEntity> {
     public static final int COIN_SLOTS = Coin.values().length;
@@ -37,10 +36,11 @@ public class SubscriptionDepositorMenu extends MenuBase<SubscriptionDepositorBlo
     @Override
     protected SubscriptionDepositorBlockEntity createOnClient(RegistryFriendlyByteBuf extraData) {
         ClientLevel world = Minecraft.getInstance().level;
+        assert world != null;
         BlockEntity blockEntity = world.getBlockEntity(extraData.readBlockPos());
-        if (blockEntity instanceof SubscriptionDepositorBlockEntity brassDepositorBE) {
-            brassDepositorBE.readClient(extraData.readNbt(), extraData.registryAccess());
-            return brassDepositorBE;
+        if (blockEntity instanceof SubscriptionDepositorBlockEntity subscriptionDepositorBE) {
+            subscriptionDepositorBE.readClient(Objects.requireNonNull(extraData.readNbt()), extraData.registryAccess());
+            return subscriptionDepositorBE;
         }
         return null;
     }
@@ -53,28 +53,9 @@ public class SubscriptionDepositorMenu extends MenuBase<SubscriptionDepositorBlo
         int x = 11+26;
         int y = 122;
 
-        for (Coin coin : Coin.values()) {
-            x += 18;
-            addSlot(new SlotDiscreteCoinBag(contentHolder.inventory, coin, x, y, true, true));
-        }
         addSlot(new CardSlot.BoundCardSlot(contentHolder.cardContainer, 0, 11+26, y)); // make here to preserve slot order
 
         addPlayerSlots(31+13, 165);
-
-        // label coins
-
-        int labelX1 = 12+13;
-        int labelX2 = labelX1 + 86;
-        int labelY = 46;
-        int labelYIncrement = 22;
-
-        for (int i = 0; i < 6; i++) {
-            Coin coin = Coin.values()[i];
-            int slotX = i < 3 ? labelX1 : labelX2;
-            int slotY = labelY + ((i%3) * labelYIncrement);
-
-            addSlot(new CoinDisplaySlot(coin, slotX, slotY));
-        }
     }
 
     @Override
@@ -94,27 +75,7 @@ public class SubscriptionDepositorMenu extends MenuBase<SubscriptionDepositorBlo
             return ItemStack.EMPTY;
         ItemStack stack = clickedSlot.getItem();
 
-        if (index < COIN_SLOTS) { // extracting coins
-            if (!(stack.getItem() instanceof CoinItem coinItem))
-                return ItemStack.EMPTY;
-
-            Coin coin = coinItem.coin;
-            int startCount = stack.getCount();
-
-            moveItemStackTo(stack, PLAYER_INV_START_INDEX, PLAYER_INV_END_INDEX, false);
-
-            int diff = startCount - stack.getCount();
-
-            if (diff > 0) {
-                contentHolder.inventory.subtract(coin, diff);
-                clickedSlot.setChanged();
-            } else if (diff < 0) {
-                contentHolder.inventory.add(coin, -diff);
-                clickedSlot.setChanged();
-            }
-
-            return ItemStack.EMPTY;
-        } else if (index == CARD_SLOT_INDEX) { // removing card
+        if (index == CARD_SLOT_INDEX) { // removing card
             if (!moveItemStackTo(stack, PLAYER_INV_START_INDEX, PLAYER_INV_END_INDEX, false))
                 return ItemStack.EMPTY;
         } else { // player inventory
